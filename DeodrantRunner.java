0bd0e4416a19a9ca68963ff5fa397dbc463559d7
Viveksangam <vivek.xworkz@gmail.com>
2021-10-05 23:45:02 +0530

    deodrant price

---
+++ DeodrantRunner.java
@@ -0,0 +1,22 @@
+class DeodrantRunner{
+public static void main(String[] values)
+{
+Deodrant deodrant=new Deodrant();
+String brand=deodrant.brand;
+{
+System.out.println(brand);
+}
+String price=deodrant.price;
+{
+System.out.println(price);
+}
+String quantity=deodrant.quantity;
+{
+System.out.println(quantity);
+}
+String color=deodrant.color;
+{
+System.out.println(color);
+}
+}
+}
