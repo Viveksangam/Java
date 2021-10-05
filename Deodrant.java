69bb0f4ec3966a36baa8059e728e41f4fa322ae4
Viveksangam <vivek.xworkz@gmail.com>
2021-10-05 23:32:44 +0530

    deodrant price and quality

---
+++ Deodrant.java
@@ -0,0 +1,18 @@
+class Deodrant{
+ String brand;
+ {
+ System.out.println("invoked brand");
+ }
+ String price;
+ {
+ System.out.println("invoked price");
+ }
+ String quantity;
+ {
+ System.out.println("invoked quantity");
+ }
+ String color;
+ {
+System.out.println("invoked color");
+} 
+}
