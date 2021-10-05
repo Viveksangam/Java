2f7be231fcdfb1bcb4e715c1cefac8f325abeace
Viveksangam <vivek.xworkz@gmail.com>
2021-10-05 23:43:54 +0530

    powerbank charging

---
+++ PowerBank.java
@@ -0,0 +1,14 @@
+class PowerBank{
+String capacity;
+{
+System.out.println("invoked charging");
+}
+String company;
+{
+	System.out.println("invoked company");
+}
+String price;
+{
+	System.out.println("invoked price");
+}
+}
---
+++ PowerBankRunner.java
@@ -0,0 +1,18 @@
+class PowerBankRunner{
+public static void main(String[] values)
+{
+PowerBank powerbank=new PowerBank();
+String capacity=powerbank.capacity;
+{
+System.out.println(capacity);
+}
+String company=powerbank.company;
+{
+System.out.println(company);
+}
+String price=powerbank.price;
+{
+System.out.println(price);
+}
+}
+}
