25ddab3ad3cbce03f8d7f494d5c63f8b90dde184
Viveksangam <vivek.xworkz@gmail.com>
2021-10-05 23:42:37 +0530

    wallet quality

---
+++ Wallet.java
@@ -0,0 +1,14 @@
+class Wallet{
+String price;
+{
+System.out.println("invoked price");
+}
+String color;
+{
+System.out.println("invoked color");
+}
+String countrymade;
+{
+System.out.println("invoked countrymade");
+}
+}
---
+++ WalletRunner.java
@@ -0,0 +1,18 @@
+class WalletRunner{
+public static void main(String[] values)
+{
+Wallet wallet=new Wallet();
+String price=wallet.price;
+{
+System.out.println(price);
+}
+String color=wallet.color;
+{
+System.out.println(color);
+}
+String countrymade=wallet.countrymade;
+{
+System.out.println(countrymade);
+}
+}
+}
