614818abdfcd04bff8aab27aa88f5b4fa094ee57
Viveksangam <vivek.xworkz@gmail.com>
2021-10-05 23:36:59 +0530

    use of internet

---
+++ InternetRunner.java
@@ -0,0 +1,10 @@
+class InternetRunner{
+public static void main(String[] values)
+{
+Internet internet=new Internet();
+String provider="Airtel";
+String Service=internet.provider;
+System.out.println(provider);
+System.out.println(Service);
+}
+}
