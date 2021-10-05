07e7d103bc89fd852dafbdedf89a31ce6bec5435
Viveksangam <vivek.xworkz@gmail.com>
2021-10-05 11:56:58 +0530

    develope application

---
+++ Developer.java
@@ -0,0 +1,26 @@
+class Developer{
+public static void writeCode()
+{
+System.out.println("invoked writecode");
+}
+public static void developApplication()
+{
+System.out.println("invoked developapplication");
+}
+public static void compile()
+{
+System.out.println("invoked compile");
+}
+public static void run()
+{
+System.out.println("invoked run");
+}
+public static void testing()
+{
+System.out.println("invoked testing");
+}
+public static void debug()
+{
+System.out.println("invoked debug");
+}
+}
